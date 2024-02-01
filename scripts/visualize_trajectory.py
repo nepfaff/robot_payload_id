f16dfe46d07d347aa066b9db5218d999762a57ec
nepfaff <nepfaff@mit.edu>
2024-01-31 23:18:53 -0500

    Extend visualize_trajectory.py to be able to visualize B-spline trajectories

--- scripts/visualize_trajectory.py
+++ scripts/visualize_trajectory.py
@@ -1,15 +1,17 @@
 import argparse
+import os
 
 from pathlib import Path
 
 import numpy as np
 
-from pydrake.all import Simulator
+from pydrake.all import BsplineBasis, BsplineTrajectory, Simulator
 
 from robot_payload_id.data import (
     compute_autodiff_joint_data_from_fourier_series_traj_params1,
 )
 from robot_payload_id.environment import create_arm
+from robot_payload_id.utils import JointData
 
 
 def main():
@@ -24,9 +26,8 @@ def main():
         type=Path,
         required=True,
         help="Path to the trajectory parameter folder. The folder must contain "
-        + "'a_value.npy', 'b_value.npy', and 'q0_value.npy'. If "
-        + "--remove_unidentifiable_params is set, the folder must also contain "
-        + "'base_param_mapping.npy'.",
+        + "'a_value.npy', 'b_value.npy', and 'q0_value.npy' or 'control_points.npy', "
+        + "'knots.npy', and 'spline_order.npy'.",
     )
     parser.add_argument(
         "--num_timesteps",
@@ -43,6 +44,8 @@ def main():
         + "the optimal experiment design parameter.",
     )
     args = parser.parse_args()
+    num_timesteps = args.num_timesteps
+    traj_parameter_path = args.traj_parameter_path
 
     # Create arm
     num_joints = 1 if args.use_one_link_arm else 7
@@ -56,19 +59,49 @@ def main():
     )
 
     # Load trajectory parameters
-    traj_parameter_path = args.traj_parameter_path
-    a_data = np.load(traj_parameter_path / "a_value.npy").reshape((num_joints, -1))
-    b_data = np.load(traj_parameter_path / "b_value.npy").reshape((num_joints, -1))
-    q0_data = np.load(traj_parameter_path / "q0_value.npy")
+    is_fourier_series = os.path.exists(traj_parameter_path / "a_value.npy")
+    if is_fourier_series:
+        a_data = np.load(traj_parameter_path / "a_value.npy").reshape((num_joints, -1))
+        b_data = np.load(traj_parameter_path / "b_value.npy").reshape((num_joints, -1))
+        q0_data = np.load(traj_parameter_path / "q0_value.npy")
 
-    joint_data = compute_autodiff_joint_data_from_fourier_series_traj_params1(
-        plant=arm_components.plant,
-        num_timesteps=args.num_timesteps,
-        time_horizon=args.time_horizon,
-        a=a_data,
-        b=b_data,
-        q0=q0_data,
-    )
+        joint_data = compute_autodiff_joint_data_from_fourier_series_traj_params1(
+            plant=arm_components.plant,
+            num_timesteps=num_timesteps,
+            time_horizon=args.time_horizon,
+            a=a_data,
+            b=b_data,
+            q0=q0_data,
+        )
+    else:
+        control_points = np.load(traj_parameter_path / "control_points.npy")
+        knots = np.load(traj_parameter_path / "knots.npy")
+        spline_order = int(np.load(traj_parameter_path / "spline_order.npy")[0])
+
+        traj = BsplineTrajectory(
+            basis=BsplineBasis(order=spline_order, knots=knots),
+            control_points=control_points,
+        )
+
+        # Sample the trajectory
+        q_numeric = np.empty((num_timesteps, num_joints))
+        q_dot_numeric = np.empty((num_timesteps, num_joints))
+        q_ddot_numeric = np.empty((num_timesteps, num_joints))
+        sample_times_s = np.linspace(
+            traj.start_time(), traj.end_time(), num=num_timesteps
+        )
+        for i, t in enumerate(sample_times_s):
+            q_numeric[i] = traj.value(t).flatten()
+            q_dot_numeric[i] = traj.EvalDerivative(t, derivative_order=1).flatten()
+            q_ddot_numeric[i] = traj.EvalDerivative(t, derivative_order=2).flatten()
+
+        joint_data = JointData(
+            joint_positions=q_numeric,
+            joint_velocities=q_dot_numeric,
+            joint_accelerations=q_ddot_numeric,
+            joint_torques=np.zeros_like(q_numeric),
+            sample_times_s=sample_times_s,
+        )
 
     simulator = Simulator(arm_components.diagram)
     simulator.set_target_realtime_rate(1.0)
