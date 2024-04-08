ccb6f58ee958d9f17f2b5351208a3bdc549ad094
nepfaff <nepfaff@mit.edu>
2024-04-08 11:03:37 -0400

    Fix missing import

--- robot_payload_id/utils/__init__.py
+++ robot_payload_id/utils/__init__.py
@@ -4,7 +4,10 @@ from .dataset import SystemIdDataset, split_dataset_into_train_val_test
 from .filtering import filter_time_series_data, process_joint_data
 from .inertia import change_inertia_reference_points_with_parallel_axis_theorem
 from .param_getters import get_plant_joint_params
-from .plant_param_setters import write_parameters_to_plant
+from .plant_param_setters import (
+    write_parameters_to_plant,
+    write_parameters_to_rigid_body,
+)
 from .utils import (
     flatten_list,
     get_package_xmls,
